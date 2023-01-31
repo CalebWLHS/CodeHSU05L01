4bdf90675ac085106459cced5d53103cf2e000a6
CalebWLHS <113708867+CalebWLHS@users.noreply.github.com>
2023-01-31 09:23:17 -0800

    Added Constructors

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,13 +2,26 @@ import java.text.DecimalFormat;
 public class Rectangle { //make public for CS A
 
     //instance variables
+    private double width;
+    private double length;
+    private double area;
+    private double perimeter;
 
     //default constructor (make public for CS A)
+    public Rectangle(){
+        width = 1.0 ;
+        length = 1.0;
+    }
 
 
     //overloaded constructor (with parameters)
-
-
+    public Rectangle(double w, double l){
+        width = w;
+        length = l;
+    }
+    public Rectangle(double width){
+        this.width = width;
+    }
 
     //accessors
 
